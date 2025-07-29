34d698fdfe5c63252ecd82d31194b97581921de4
Abootlha <mdzaidtalha6393@gmail.com>
2025-07-29 23:13:03 +0530

    update dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.BASE_PATH || '',
   server: {
     host: "0.0.0.0",
     port: 5173,
